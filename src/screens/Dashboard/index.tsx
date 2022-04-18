4965da3e561b17b66709f1673474e18859403bfd
Alexandre Castro <alexandre.acrsf@gmail.com>
2022-04-18 10:04:19 -0300

    Tipando e adicionando propriedades com styled-components

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { HighlightCard } from '../../Components/HighLightCard';
 
 import {
   Container,
@@ -10,6 +11,7 @@ import {
   User,
   UserGreeting,
   UserName,
+  HighlightCards,
 } from './styles';
 
  export function Dashboard(){
@@ -30,10 +32,30 @@ import {
           </UserInfo>
       
           <Icon name='power'/>
-        </UserWrapper>
-
-       
+        </UserWrapper>  
       </Header>
+      
+      <HighlightCards>
+        <HighlightCard
+          type='up'
+          title='Entradas' 
+          amount='R$ 17.400,00'
+          lastTransaction='Última entrada dia 13 de abril'
+        />
+        <HighlightCard
+          type='down'
+          title='Saídas' 
+          amount='R$ 17.400,00'
+          lastTransaction='Última entrada dia 13 de abril'
+        />
+        <HighlightCard
+          type='total'
+          title='Total' 
+          amount='R$ 17.400,00'
+          lastTransaction='Última entrada dia 13 de abril'
+        />
+      </HighlightCards>
+      
     </Container>
   );
  }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,8 +1,7 @@
 import styled from "styled-components/native";
-
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
-
 import { Feather } from '@expo/vector-icons';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
     flex: 1;
@@ -15,13 +14,15 @@ export const Header = styled.View`
     background-color: ${({ theme }) => theme.colors.primary};
     
     justify-content: center;
-    align-items: center;
+    align-items: flex-start;
+    flex-direction: row;
 `;
 
 export const UserWrapper = styled.View`
     width: 100%;
 
     padding: 0 24px;
+    margin-top: ${getStatusBarHeight() + 24}px;
 
     flex-direction: row;
     justify-content: space-between;
@@ -61,4 +62,15 @@ export const UserName = styled.Text`
 
     font-size: ${RFValue(18)}px;
     font-family: ${({ theme }) => theme.fonts.bold};
+`;
+
+export const HighlightCards = styled.ScrollView.attrs({
+    horizontal: true,
+    showsHorizontalScrollIndicator: false,
+    paddingHorizontal: 24
+})`
+    width: 100%;
+
+    position: absolute;
+    margin-top: ${RFPercentage(20)}px;
 `;
