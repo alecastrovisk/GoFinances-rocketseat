85c5208a4f3d1d384da3526711c8ebad7e9aadee
Alexandre Castro <alexandre.acrsf@gmail.com>
2022-04-23 09:11:50 -0300

    styled-components

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 import { HighlightCard } from '../../Components/HighLightCard';
+import { TransactionCard, TransactionCardProps } from '../../Components/TransactionCard';
 
 import {
   Container,
@@ -12,9 +14,50 @@ import {
   UserGreeting,
   UserName,
   HighlightCards,
+  Transactions,
+  Title,
+  TransactionsList
 } from './styles';
 
- export function Dashboard(){
+export interface DataListProps extends TransactionCardProps {
+  id: string;
+}
+
+ export function Dashboard(){ 
+  const data: DataListProps[] = [{
+    id: '1',
+    type: 'positive',
+    title:'Desenvolvimento de site',
+    amount:'R$ 12.000',
+    category: {
+      name: 'Vendas',
+      icon: 'dollar-sign'
+    },   
+    date:'22/04/2022'
+  },
+  {
+    id: '2',
+    type: 'negative',
+    title:'Mel',
+    amount:'R$1.200',
+    category: {
+      name: 'Alimentação',
+      icon: 'coffee'
+    },   
+    date:'22/04/2022'
+  },
+  {
+    id: '3',
+    type: 'negative',
+    title:'PC NOVO',
+    amount:'R$ 12.000',
+    category: {
+      name: 'Vendas',
+      icon: 'shopping-bag'
+    },   
+    date:'22/04/2022'
+  }
+]
   return(
     <Container>
       <Header>
@@ -55,6 +98,20 @@ import {
           lastTransaction='Última entrada dia 13 de abril'
         />
       </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+
+        <TransactionsList
+          data={data}
+          keyExtractor={ item => item.id}
+          renderItem={({ item }) => (
+            <TransactionCard
+              data={item}
+            />
+          )}
+        />
+      </Transactions>
       
     </Container>
   );
